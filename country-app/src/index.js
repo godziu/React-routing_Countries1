565159005aa2159f9bb86f65b7c70f5977734df4
Damian Godek <damian.godek1@gmail.com>
2018-12-04 19:45:55 +0100

    fix devTools

--- country-app/src/index.js
+++ country-app/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+import DevTools from './DevTools';
+import { getCountries } from '../src/actions/actions-countries';
+
 
 render(
     <Provider store={store}>
